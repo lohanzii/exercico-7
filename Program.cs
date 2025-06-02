279db030cd9419ff3971f5ecfa102916b1ea06a9
biancaFers <biancaferreiras1997@gmail.com>
2025-06-02 19:39:13 -0300

    primeiro commit

--- Program.cs
+++ Program.cs
@@ -7,3 +7,5 @@ Console.WriteLine("Informe o mês");
 mes = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Ja se passram " + mes * 30 + " de dias desde o incio do ano");
+
+dia = Convert.ToInt32(Console.ReadLine());
