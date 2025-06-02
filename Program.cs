5e0d00679876846b95839bd56fe2a317116420f4
biancaFers <biancaferreiras1997@gmail.com>
2025-06-02 19:37:28 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int dia, mes;
+
+Console.WriteLine("Informe o dia");
+dia = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o mês");
+mes = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ja se passram " + mes * 30 + " de dias desde o incio do ano");
